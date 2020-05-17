3e1a52465f62220845573d97abc0a828fc428aae
Minifranger <clement.franger@gmail.com>
2020-05-17 13:13:48 +0200

    first draw layers

---
+++ aws_utils/dynamodb/create.py
@@ -0,0 +1,34 @@
+import json
+import logging
+import boto3
+from aws_utils.utils import DecimalEncoder, success, failure, validate_params
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+client = boto3.resource('dynamodb')
+
+
+def create_reminder(event, context, table):
+    logger.info('event : {event}'.format(event=event))
+    body, = validate_params(body=event.get('body'))
+
+    body = json.loads(body) if isinstance(body, str) else body
+    if not body.get('id'):
+        return failure(code=400, body='You should provide a reminder id to your payload')
+
+    params = {
+        'TableName': table,
+        'Item': body
+    }
+
+    logger.info('Creating item {id}'.format(id=body.get('id')))
+
+    try:
+        client.Table(params.get('TableName')).put_item(**params)
+    except Exception as e:
+        return failure(body=e)
+
+    logger.info('Created item {id}'.format(id=id))
+    return success(body=json.dumps(params['Item'], cls=DecimalEncoder))
