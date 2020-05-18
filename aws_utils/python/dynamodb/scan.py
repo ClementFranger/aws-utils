426593f963f6b8b62c64db1fbca1a6391f51a75f
Minifranger <clement.franger@gmail.com>
2020-05-18 18:36:21 +0200

    scan first draw

---
+++ aws_utils/python/dynamodb/scan.py
@@ -0,0 +1,34 @@
+import json
+import logging
+import boto3
+from utils import DecimalEncoder, success, failure, validate_params
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+client = boto3.resource('dynamodb')
+
+
+def scan(event, context, table):
+    logger.info('event : {event}'.format(event=event))
+    # path, = validate_params(path=event.get('pathParameters'))
+
+    # id = path.get('id')
+    # if not id:
+    #     return failure(code=400, body='You should provide a id to your path parameters')
+
+    params = {
+        'TableName': table
+    }
+
+    logger.info('Getting all items')
+
+    try:
+        result = client.Table(params.get('TableName')).scan(**params)
+        # if not result.get('Items'):
+        #     return success(status_code=204, body=json.dumps(result.get('Items'), cls=DecimalEncoder))
+    except Exception as e:
+        return failure(body=e)
+
+    logger.info('Retrieved all items')
+    return success(body=json.dumps(result.get('Items'), cls=DecimalEncoder))
