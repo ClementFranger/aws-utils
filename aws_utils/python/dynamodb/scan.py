b999413d860ff204546cc0a3f9a1642f0019f317
Minifranger <clement.franger@gmail.com>
2020-05-19 13:37:17 +0200

    filter option of scan

--- aws_utils/python/dynamodb/scan.py
+++ aws_utils/python/dynamodb/scan.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import boto3
+
 from utils import DecimalEncoder, success, failure, validate_params
 
 logger = logging.getLogger(__name__)
@@ -9,7 +10,7 @@ logger.setLevel(logging.INFO)
 client = boto3.resource('dynamodb')
 
 
-def scan(event, context, table):
+def scan(event, context, **kwargs):
     logger.info('event : {event}'.format(event=event))
     # path, = validate_params(path=event.get('pathParameters'))
 
@@ -18,15 +19,18 @@ def scan(event, context, table):
     #     return failure(code=400, body='You should provide a id to your path parameters')
 
     params = {
-        'TableName': table
+        'TableName': kwargs.get('table')
     }
+
+    if kwargs.get('filter'):
+        params.update({'FilterExpression': kwargs.get('filter')})
 
     logger.info('Getting all items')
 
     try:
         result = client.Table(params.get('TableName')).scan(**params)
-        # if not result.get('Items'):
-        #     return success(status_code=204, body=json.dumps(result.get('Items'), cls=DecimalEncoder))
+        if not result.get('Items'):
+            return success(status_code=204, body=json.dumps(result.get('Items'), cls=DecimalEncoder))
     except Exception as e:
         return failure(body=e)
 
