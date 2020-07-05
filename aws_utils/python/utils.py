4dd2786d68ec198e4ffd01bcd760368d5ded018c
Minifranger <clement.franger@gmail.com>
2020-07-05 11:46:26 +0200

    any cors in list of ips

--- aws_utils/python/utils.py
+++ aws_utils/python/utils.py
@@ -71,7 +71,7 @@ def cors(ips):
         @wraps(f)
         def wrapper(event, context, *args, **kwargs):
             logger.info('event : {event}'.format(event=event))
-            if re.compile(ips).match(event.get('headers').get('origin')):
+            if any(re.compile(ip).match(event.get('headers').get('origin')) for ip in ips):
                 kwargs.update({'headers': {'Access-Control-Allow-Origin': event.get('headers').get('origin')}})
             return f(event, context, *args, **kwargs)
         return wrapper
