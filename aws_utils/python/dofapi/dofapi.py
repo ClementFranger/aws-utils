9f2c0889150f4cd84f603c9baed70da1c08ad788
Minifranger <clement.franger@gmail.com>
2020-07-05 20:37:51 +0200

    test init

--- aws_utils/python/dofapi/dofapi.py
+++ aws_utils/python/dofapi/dofapi.py
@@ -8,11 +8,14 @@ logger.setLevel(logging.INFO)
 
 class Dofapi(object):
     __DOFAPI_API_ = 'https://fr.dofus.dofapi.fr/'
-    __WEAPONS__ = 'weapons/44'
+    __WEAPONS__ = 'weapons'
+
+    def __init__(self):
+        pass
 
     @request
     @to_json
-    def get_all_weapons(self, *args, **kwargs):
+    def scan_weapons(self, *args, **kwargs):
         logger.info('Getting all weapons')
 
         result = requests.get(self.__DOFAPI_API_ + self.__WEAPONS__, *args, **kwargs)
