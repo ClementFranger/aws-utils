3b15c25f42dd2ed111675c742bbe39224d83e414
Minifranger <clement.franger@gmail.com>
2020-07-10 15:24:43 +0200

    added cors decorator

--- aws_utils/python/lambdas/lambdas.py
+++ aws_utils/python/lambdas/lambdas.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 from functools import wraps
 
 logger = logging.getLogger(__name__)
@@ -25,15 +26,25 @@ class Lambdas(object):
                     logger.info('Received payload : {payload}'.format(payload=event.get('body')))
 
                     body = json.loads(event.get('body')) if event.get('body') else dict()
-
                     if body and not body.get(id):
-                        return 400, 'You should provide a {id} key to your body'.format(id=id)
+                        raise ValueError('You should provide a {id} key to your body'.format(id=id))
                     if event.get('pathParameters') and not event.get('pathParameters').get(id):
-                        return 400, 'You should provide a {id} key to your pathParameters'.format(id=id)
+                        raise ValueError('You should provide a {id} key to your pathParameters'.format(id=id))
                     kwargs.update({'body': body, 'path': event.get('pathParameters'),
                                    'query': event.get('queryStringParameters')})
 
                     logger.info('Payload have been loaded into kwargs : {kwargs}'.format(kwargs=kwargs))
+                    return f(event, *args, **kwargs)
+                return wrapper
+            return decorator
+
+        @classmethod
+        def cors(cls, ips):
+            def decorator(f):
+                @wraps(f)
+                def wrapper(event, *args, **kwargs):
+                    if any(re.compile(ip).match(event.get('headers').get('origin')) for ip in ips):
+                        kwargs.update({'headers': {'Access-Control-Allow-Origin': event.get('headers').get('origin')}})
                     return f(event, *args, **kwargs)
                 return wrapper
             return decorator
