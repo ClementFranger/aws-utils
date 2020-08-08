1652be9ad3383cfab07537eedbeab0d99fe05acd
Minifranger <clement.franger@gmail.com>
2020-08-08 23:12:06 +0200

    added some refacto schema from api

--- aws_utils/python/dofapi/dofapi.py
+++ aws_utils/python/dofapi/dofapi.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from functools import wraps
 
 import requests
@@ -19,6 +20,68 @@ class Dofapi(object):
     __RESOURCES__ = 'resources'
     __WEAPONS__ = 'weapons'
 
+    class APISchema(object):
+        CLASSES = 'classes'
+        CONSUMABLES = 'consumables'
+        EQUIPMENTS = 'equipments'
+        HARNESSES = 'harnesses'
+        HAVENBAGS = 'havenbags'
+        IDOLS = 'idols'
+        MONSTERS = 'monsters'
+        MOUNTS = 'mounts'
+        PETS = 'pets'
+        PROFESSIONS = 'professions'
+        RESOURCES = 'resources'
+        SETS = 'sets'
+        WEAPONS = 'weapons'
+
+    class WeaponsSchema(object):
+        ARC = 'Arc'
+        EPEE = 'Épée'
+        DAGUE = 'Dague'
+        BAGUETTE = 'Baguette'
+        BATON = 'Bâton'
+        MARTEAU = 'Marteau'
+        PELLE = 'Pelle'
+        HACHE = 'Hache'
+        OUTIL = 'Outil'
+        FAUX = 'Faux'
+        PIOCHE = 'Pioche'
+        PIERRE_AME = "Pierre d'âme"
+
+    class EquipmentsSchema(object):
+        AMULETTE = 'Amulette'
+        ANNEAU = 'Anneau'
+        BOTTES = 'Bottes'
+        CEINTURE = 'Ceinture'
+        CHAPEAU = 'Chapeau'
+        CAPE = 'Cape'
+        DOFUS = 'Dofus'
+        SAC_DOS = 'Sac à dos'
+        BOUCLIER = 'Bouclier'
+        OBJET_VIVANT = 'Objet vivant'
+        TROPHEE = 'Trophée'
+
+    class ResourcesSchema(object):
+        PLANCHE = 'Planche'
+        SUBSTRAT = 'Substrat'
+        ALLIAGE = 'Alliage'
+        PIERRE_PRECIEUSE = 'Pierre précieuse'
+        ESSENCE = 'Essence de gardien de donjon'
+        TEINTURE = 'Teinture'
+        IDOLE = 'Idole'
+        CLEF = 'Clef'
+
+    class ConsumablesSchema(object):
+        POTION = 'Potion'
+        POTION_TELEPORTATION = 'Potion de téléportation'
+        POTION_OUBLI_PERCEPTEUR = 'Potion d\'oubli Percepteur'
+        POTION_CONQUETE = 'Potion de conquête'
+        OBJET_ELEVAGE = 'Objet d\'élevage'
+
+    class Schema(APISchema, WeaponsSchema, EquipmentsSchema, ResourcesSchema, ConsumablesSchema):
+        pass
+
     class Decorators(object):
         @classmethod
         def output(cls, f):
@@ -27,6 +90,7 @@ class Dofapi(object):
                 return f(*args, **kwargs).json()
             return wrapper
 
+    @Decorators.output
     def _scan(self, *args, **kwargs):
         endpoint, unique = kwargs.get('endpoint', self.__RESOURCES__), kwargs.get('unique', False)
 
@@ -37,23 +101,23 @@ class Dofapi(object):
         logger.info('Retrieved {endpoint}'.format(endpoint=endpoint))
         return list({v[self.__ID__]: v for v in result}.values()) if unique else result
 
-    @Decorators.output
+    # @Decorators.output
     def scan_consumables(self, *args, **kwargs):
         return self._scan(endpoint=self.__CONSUMABLES__, *args, **kwargs)
 
-    @Decorators.output
+    # @Decorators.output
     def scan_equipments(self, *args, **kwargs):
         return self._scan(endpoint=self.__EQUIPMENTS__, *args, **kwargs)
 
-    @Decorators.output
+    # @Decorators.output
     def scan_idols(self, *args, **kwargs):
         return self._scan(endpoint=self.__IDOLS__, *args, **kwargs)
 
-    @Decorators.output
+    # @Decorators.output
     def scan_resources(self, *args, **kwargs):
         return self._scan(endpoint=self.__RESOURCES__, *args, **kwargs)
 
-    @Decorators.output
+    # @Decorators.output
     def scan_weapons(self, *args, **kwargs):
         return self._scan(endpoint=self.__WEAPONS__, *args, **kwargs)
 
@@ -71,3 +135,6 @@ class Dofapi(object):
 
         logger.info('Retrieved all items')
         return result
+
+    def _scan_professions(self, *args, **kwargs):
+        return self._scan(endpoint=self.APISchema.PROFESSIONS, *args, **kwargs)
