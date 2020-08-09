e01f6dfa084eb874d68f8264c140216b70a6ea39
Minifranger <clement.franger@gmail.com>
2020-08-09 17:53:14 +0200

    some refacto

--- aws_utils/python/dofapi/dofapi.py
+++ aws_utils/python/dofapi/dofapi.py
@@ -9,16 +9,16 @@ logger.setLevel(logging.INFO)
 
 
 class Dofapi(object):
-    __ID__ = '_id'
-    __ANKAMA_ID__ = 'ankamaId'
-    __NAME__ = 'name'
+    # __ID__ = '_id'
+    # __ANKAMA_ID__ = 'ankamaId'
+    # __NAME__ = 'name'
 
     __DOFAPI_API_ = 'https://fr.dofus.dofapi.fr/'
-    __CONSUMABLES__ = 'consumables'
-    __EQUIPMENTS__ = 'equipments'
-    __IDOLS__ = 'idols'
-    __RESOURCES__ = 'resources'
-    __WEAPONS__ = 'weapons'
+
+    class IDSchema(object):
+        ID = '_id'
+        ANKAMA_ID = 'ankamaId'
+        NAME = 'name'
 
     class APISchema(object):
         CLASSES = 'classes'
@@ -79,47 +79,36 @@ class Dofapi(object):
         POTION_CONQUETE = 'Potion de conquête'
         OBJET_ELEVAGE = 'Objet d\'élevage'
 
-    class Schema(APISchema, WeaponsSchema, EquipmentsSchema, ResourcesSchema, ConsumablesSchema):
+    class Schema(IDSchema, APISchema, WeaponsSchema, EquipmentsSchema, ResourcesSchema, ConsumablesSchema):
         pass
 
-    class Decorators(object):
-        @classmethod
-        def output(cls, f):
-            @wraps(f)
-            def wrapper(*args, **kwargs):
-                return f(*args, **kwargs).json()
-            return wrapper
-
-    @Decorators.output
     def _scan(self, *args, **kwargs):
-        endpoint, unique = kwargs.get('endpoint', self.__RESOURCES__), kwargs.get('unique', False)
+        endpoints, unique = kwargs.get('endpoints'), kwargs.get('unique', False)
+        endpoints = endpoints if isinstance(endpoints, list) else [endpoints]
 
-        logger.info('Getting all {endpoint}'.format(endpoint=endpoint))
+        result = []
+        for endpoint in endpoints:
+            logger.info('Getting all {endpoint}'.format(endpoint=endpoint))
 
-        result = requests.get(self.__DOFAPI_API_ + endpoint, *args)
+            result.extend(requests.get(self.__DOFAPI_API_ + endpoint, *args).json())
 
-        logger.info('Retrieved {endpoint}'.format(endpoint=endpoint))
-        return list({v[self.__ID__]: v for v in result}.values()) if unique else result
+            logger.info('Retrieved {endpoint}'.format(endpoint=endpoint))
+        return list({v[self.Schema.ID]: v for v in result}.values()) if unique else result
 
-    # @Decorators.output
     def scan_consumables(self, *args, **kwargs):
-        return self._scan(endpoint=self.__CONSUMABLES__, *args, **kwargs)
+        return self._scan(endpoints=self.APISchema.CONSUMABLES, *args, **kwargs)
 
-    # @Decorators.output
     def scan_equipments(self, *args, **kwargs):
-        return self._scan(endpoint=self.__EQUIPMENTS__, *args, **kwargs)
+        return self._scan(endpoints=self.APISchema.EQUIPMENTS, *args, **kwargs)
 
-    # @Decorators.output
     def scan_idols(self, *args, **kwargs):
-        return self._scan(endpoint=self.__IDOLS__, *args, **kwargs)
+        return self._scan(endpoints=self.APISchema.IDOLS, *args, **kwargs)
 
-    # @Decorators.output
     def scan_resources(self, *args, **kwargs):
-        return self._scan(endpoint=self.__RESOURCES__, *args, **kwargs)
+        return self._scan(endpoints=self.APISchema.RESOURCES, *args, **kwargs)
 
-    # @Decorators.output
     def scan_weapons(self, *args, **kwargs):
-        return self._scan(endpoint=self.__WEAPONS__, *args, **kwargs)
+        return self._scan(endpoints=self.APISchema.WEAPONS, *args, **kwargs)
 
     def _scan_items(self, *args, **kwargs):
         unique = kwargs.get('unique', False)
@@ -130,11 +119,11 @@ class Dofapi(object):
 
         if unique:
             logger.info('length of NON UNIQUE items : %s' % len(result))
-            result = list({v[self.__ID__]: v for v in result}.values())
+            result = list({v[self.Schema.ID]: v for v in result}.values())
             logger.info('length of UNIQUE items : %s' % len(result))
 
         logger.info('Retrieved all items')
         return result
 
     def _scan_professions(self, *args, **kwargs):
-        return self._scan(endpoint=self.APISchema.PROFESSIONS, *args, **kwargs)
+        return self._scan(endpoints=self.APISchema.PROFESSIONS, *args, **kwargs)
